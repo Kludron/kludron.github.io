42a122885aaa1928a15a98f992af35c62e37355d
Kludron <kludron@lukewarmsecurityinfo.com>
2022-09-22 14:33:49 +1000

    Added more verbosity

--- tools/localrun.sh
+++ tools/localrun.sh
@@ -5,9 +5,11 @@ rootpath=$(pwd)/..
 
 # Remove old files
 if [ -d "$rootpath/_site" ]; then
-    echo "Removing old site..."
+    echo -n "Removing old site..."
     rm -r "$rootpath/_site"
+    echo " Removed"
 fi 
 
 # Start Docker instance; then
+echo "Starting docker instance..."
 docker run --name $sitename --rm -e JEKYLL_ENV=development --volume="$rootpath:/srv/jekyll" -p 80:80 -it jekyll/jekyll jekyll serve --watch --port 80
